183cbc8bef566615b7d95d4a3a5871b31a3501ef
Gabriel Galvao <gabriel.mgalvao38@gmail.com>
2025-03-15 13:25:55 -0700

    Added firebase to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Auth, signInWithPopup, GoogleAuthProvider, signOut } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'kanban-ng';
+  constructor(private auth: Auth) {}
+
+  login() {
+    signInWithPopup(this.auth, new GoogleAuthProvider()).then((user) => {
+      console.log('User:', user);
+    });
+  }
+
+  logout() {
+    signOut(this.auth);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,42 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai';
+
+import { environment } from '../../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideFirebaseApp(() => initializeApp({ 
+      projectId: environment.firebaseConfig.projectId, 
+      appId: environment.firebaseConfig.appId, 
+      storageBucket: environment.firebaseConfig.storageBucket, 
+      apiKey: environment.firebaseConfig.apiKey, 
+      authDomain: environment.firebaseConfig.authDomain, 
+      messagingSenderId: environment.firebaseConfig.messagingSenderId, 
+      measurementId: environment.firebaseConfig.measurementId 
+    })), 
+    provideAuth(() => getAuth()), 
+    provideAnalytics(() => getAnalytics()), 
+    ScreenTrackingService, 
+    UserTrackingService,
+    provideFirestore(() => getFirestore()), 
+    provideDatabase(() => getDatabase()), 
+    provideFunctions(() => getFunctions()), 
+    provideMessaging(() => getMessaging()), 
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()), 
+    provideVertexAI(() => getVertexAI())
+  ]
 };
