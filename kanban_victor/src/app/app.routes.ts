beccd0b210b13b21e4e5b9013d9dce83e22b7b46
Victor Costa Nova <64611385+victorcostanova@users.noreply.github.com>
2025-03-16 15:08:37 -0700

    update

--- kanban_victor/src/app/app.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainViewComponent } from './pages/main-view/main-view.component';
-
-export const routes: Routes = [{ path: '', component: MainViewComponent }];
