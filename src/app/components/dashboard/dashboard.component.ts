d2ebb326481ba5abb89e09fdc00f4ab88885711d
Gabriel Galvao <gabriel.mgalvao38@gmail.com>
2025-03-20 02:37:49 -0700

    Added .angular

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [FormsModule, DragDropModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  
+  task: string = '';
+  taskDescription: string = '';
 }
