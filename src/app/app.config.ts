2dd9530dc9b2782f1bdbb508fec4fb4d4788b303
Victor Costa Nova <64611385+victorcostanova@users.noreply.github.com>
2025-03-19 20:17:58 -0700

    dashboard_draganddrop_tasks

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,39 +4,49 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { getMessaging, provideMessaging } from '@angular/fire/messaging';
 import { getPerformance, providePerformance } from '@angular/fire/performance';
-import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import {
+  getRemoteConfig,
+  provideRemoteConfig,
+} from '@angular/fire/remote-config';
 import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai';
 
 import { environment } from '../../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    provideFirebaseApp(() => initializeApp({ 
-      projectId: environment.firebaseConfig.projectId, 
-      appId: environment.firebaseConfig.appId, 
-      storageBucket: environment.firebaseConfig.storageBucket, 
-      apiKey: environment.firebaseConfig.apiKey, 
-      authDomain: environment.firebaseConfig.authDomain, 
-      messagingSenderId: environment.firebaseConfig.messagingSenderId
-    })), 
-    provideAuth(() => getAuth()), 
-    provideAnalytics(() => getAnalytics()), 
-    ScreenTrackingService, 
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: environment.projectId,
+        appId: environment.appId,
+        storageBucket: environment.storageBucket,
+        apiKey: environment.apiKey,
+        authDomain: environment.authDomain,
+        messagingSenderId: environment.messagingSenderId,
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
     UserTrackingService,
-    provideFirestore(() => getFirestore()), 
-    provideDatabase(() => getDatabase()), 
-    provideFunctions(() => getFunctions()), 
-    provideMessaging(() => getMessaging()), 
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
     providePerformance(() => getPerformance()),
-    provideRemoteConfig(() => getRemoteConfig()), 
-    provideVertexAI(() => getVertexAI()), 
-  ]
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideVertexAI(() => getVertexAI()),
+  ],
 };
