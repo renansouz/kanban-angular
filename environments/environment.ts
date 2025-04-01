56b8c63c36c1a577b64345753f8cba46378877a9
Gabriel Galvao <gabriel.mgalvao38@gmail.com>
2025-04-01 11:14:56 -0700

    Added structure to change button styles on products page

---
+++ environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyCKDdPafM1BWeY9zhkESUypodRwONW9hvc",
+      authDomain: "kanban-ng-ea027.firebaseapp.com",
+      projectId: "kanban-ng-ea027",
+      storageBucket: "kanban-ng-ea027.firebasestorage.app",
+      messagingSenderId: "531842864402",
+      appId: "1:531842864402:web:8d9ff662169c50ab164fcb"
+    }
+  };
+  
